68dc139cf0c3578c57b321ddf3e1b5164dff88fc
benianlim <43428553+benianlim@users.noreply.github.com>
2018-11-18 17:13:19 +0800

    Update servReq.php

--- servReq.php
+++ servReq.php
@@ -3,15 +3,29 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "bit216";
+$dbname = "requests_database";
 $con = new mysqli($servername, $username, $password, $dbname);
 
-$requiredDate = $_POST['requestDate'];
-$notes = $_POST['comments'];
+$requestDate = $_POST['requestDate'];
+// $requestTime = $_POST['requestTime'];
+$notes = $_POST['notes'];
 
-$sql = "INSERT INTO servicerequest (requiredDate, notes)
-        VALUES ('$requiredDate', '$notes')";
+$requestID = null;
+$status = "Pending";
+
+$sql = "INSERT INTO request_table (requestID, requestDate, notes, status)
+        VALUES ('$requestID', '$requestDate', '$notes', '$status')";
 
 mysqli_query($con, $sql);
 mysqli_close($con);
+
+/*
+if(isset($_POST['submitButton'])) {
+  $input = $_POST["requestDate"];
+  $input = $_POST["notes"];
+
+  header("Location: request.php");
+  exit();
+}
 ?>
+*/
